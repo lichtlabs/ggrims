084f1f58e9ff3a037fb24cbee9bc5f407b4c3e1c
sena <sena@lichtlabs.org>
2024-10-26 18:18:31 +0700

    feat(events-list): add loading skeletons to improve user experience during data fetching feat(skeleton): create Skeleton component for consistent loading indicators chore(prettierrc): set printWidth to 80 for better code readability

--- src/app/upcoming/components/events-list.tsx
+++ src/app/upcoming/components/events-list.tsx
@@ -5,9 +5,10 @@ import { useQuery } from "@tanstack/react-query"
 import { format } from "date-fns"
 import { createApiClient } from "@/lib/api-client"
 import type { Event } from "@/lib/types"
+import { Skeleton } from "@/components/ui/skeleton"
 
 export default function EventsList() {
-  const { data } = useQuery({
+  const { data, isLoading } = useQuery({
     queryKey: ["upcoming-events"],
     queryFn: () => createApiClient().events.ListUpcomingEvents(),
     refetchOnWindowFocus: false
@@ -17,11 +18,6 @@ export default function EventsList() {
 
   return (
     <>
-      {!events?.length && (
-        <p>
-          There are no events yet.
-        </p>
-      )}
       {events?.map((event) => (
         <li key={event.id} className="border-b border-black/[.08] pb-4">
           <div className="relative flex justify-between">
@@ -34,11 +30,25 @@ export default function EventsList() {
               <p className="text-sm">{truncate(event.description, 54)}</p>
             </div>
           </div>
-          <div className="w-full mt-4">
+          <div className="mt-4 w-full">
             <GetTicketButton eventId={event.id} />
           </div>
         </li>
       ))}
+      {isLoading &&
+        [1, 2, 3].map((i) => (
+          <li key={i} className="mb-4 border-b border-black/[.08] pb-4">
+            <div className="relative flex justify-between">
+              <div className="w-full">
+                <Skeleton className="mb-2 h-6 w-3/4" />
+                <Skeleton className="mb-2 h-4 w-1/2" />
+                <Skeleton className="mb-4 h-4 w-5/6" />
+              </div>
+            </div>
+            <Skeleton className="h-8 w-[100px]" />
+          </li>
+        ))}
+      {!isLoading && !events?.length && <p>There are no events yet.</p>}
     </>
   )
 }
